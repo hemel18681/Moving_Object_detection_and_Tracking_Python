aa5aa22bd5bd25b69f28c046cae28a788a954125
hemel18681 <hemel18103112@gmail.com>
2020-10-09 20:08:18 +0600

    work done try something new

--- main.py
+++ main.py
@@ -21,7 +21,12 @@ while True:
     threshImg = cv2.dilate(threshImg,None,iterations=2)
     cnts = cv2.findContours(threshImg.copy(),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
     cnts = imutils.grab_contours(cnts)
-
+    for c in cnts:
+        if cv2.contourArea(c) < area:
+            continue
+        (x,y,w,h) = cv2.boundingRect(c)
+        cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
+        text = "Moving Object Detected"
     print(text)
     cv2.putText(img,text,(10,20),cv2.FONT_HERSHEY_SIMPLEX,0.5,(0,0,255),2)
     cv2.imshow("my_cam",img)
