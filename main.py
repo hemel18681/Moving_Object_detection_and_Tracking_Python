0bdf27c84edda440ac3a7808dbad8a4af78781d7
hemel18681 <hemel18103112@gmail.com>
2020-10-09 20:07:48 +0600

    work more

--- main.py
+++ main.py
@@ -19,7 +19,11 @@ while True:
     imgDiff = cv2.absdiff(firstFrame,grayImg)
     threshImg = cv2.threshold(imgDiff,25,255,cv2.THRESH_BINARY)[1]
     threshImg = cv2.dilate(threshImg,None,iterations=2)
+    cnts = cv2.findContours(threshImg.copy(),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
 
+    print(text)
+    cv2.putText(img,text,(10,20),cv2.FONT_HERSHEY_SIMPLEX,0.5,(0,0,255),2)
     cv2.imshow("my_cam",img)
     key = cv2.waitKey(1) & 0xFF
     if key == ord("q"):
